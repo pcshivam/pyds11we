89d6bcb5350f78a06abfae394611374ab6c62a18
akkyboy77 <erakshaytamhane@gmail.com>
2024-02-11 00:48:52 +0530

    This is my Git file

---
+++ ketaki.py
@@ -0,0 +1,8 @@
+def odd_number(num):
+    if num%2 != 0:
+        print("Odd Number")
+
+    else:
+        print("Even Number")
+
+odd_number(25)
---
+++ ketaki_test.py
@@ -0,0 +1,8 @@
+def mult(n1, n2):
+  print("Multiplication: ", n1*n2)
+
+def div(n1, n2):
+  print("Division: ", n1/n2)
+
+mult(2,4)
+div(10,5)
